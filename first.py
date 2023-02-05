f3934aa70d2438aaeddce4af94b97fe6b066a15d
Assylzhan Arapova <124043844+assylzhaan@users.noreply.github.com>
2023-02-05 10:50:38 +0600

    Лаб 2 (3 тапсырма)
    Бағдарлама құрып студент жайында ақпарат енгізілді. Шартты операторлар мен комментарии қолданылды

---
+++ first.py
@@ -0,0 +1,10 @@
+#Arapova Assylzhan
+a = input( "ФИО: ")
+b = input("Возраст: ")
+c = input("Место жительство: ")
+print("Ваши фамилия, имя: ",a)
+print("Ваш возраст: ",b)
+print("Вы живете в: ",c)
+
+if(c =="Almaty" ): print("Almaty!!")
+else: print ("not Almaty")
