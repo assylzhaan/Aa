bd7682fec0e6047ac8b51fd4e9722815dc9da427
Assylzhan Arapova <124043844+assylzhaan@users.noreply.github.com>
2023-02-06 21:43:06 +0600

    while циклы арқылы сандарды тексеру
    .

--- 3_lab.py
+++ 3_lab.py
@@ -1,10 +1,11 @@
 #Arapova Assylzhan
 #1 for және while циклдарды қолданып жеке программа құру
-N= int(input("N="))
-for i in range (1, N+1):
+a = int(input("a="))
+i=0
+while i<a:
+    i+=1
     if i%2==0:
         print(i, "-" ,"Zhyp san")
     else:
         print(i,"-","Taq san")
-
 
