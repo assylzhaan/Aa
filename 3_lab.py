122aa7b5fa4d40f8042b46dc7518ac8aca2bdd11
Assylzhan Arapova <124043844+assylzhaan@users.noreply.github.com>
2023-02-06 21:37:48 +0600

    for операторымен программа құрдым
    Берілген санға дейінгі сандардың тақ немесе жұп екенін тексеру

---
+++ 3_lab.py
@@ -0,0 +1,10 @@
+#Arapova Assylzhan
+#1 for және while циклдарды қолданып жеке программа құру
+N= int(input("N="))
+for i in range (1, N+1):
+    if i%2==0:
+        print(i, "-" ,"Zhyp san")
+    else:
+        print(i,"-","Taq san")
+
+
--- first.py
+++ first.py
@@ -6,5 +6,5 @@ print("Ваши фамилия, имя: ",a)
 print("Ваш возраст: ",b)
 print("Вы живете в: ",c)
 
-if(c =="Almaty" ): print("Almaty!!")
-else: print ("not Almaty")
+if(c =="Almaty" ): print("Almaty!!")# студент Алматы қаласынан болса Almaty!!жазуы шығады
+else: print ("not Almaty") 
