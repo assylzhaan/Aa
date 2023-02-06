12c15800ad9473289776756d55e3e49288be856d
Assylzhan Arapova <124043844+assylzhaan@users.noreply.github.com>
2023-02-06 22:55:46 +0600

    4 лаб есептері

---
+++ 4_lab.py
@@ -0,0 +1,22 @@
+#Arapova 
+string = str(input())
+upper = 0
+lower = 0
+for i in string:
+    if i.isupper():
+        upper += 1
+    else:
+        lower += 1
+if upper > lower:
+    print(string.upper())
+else:
+    print(string.lower())
+
+
+#2
+a = input("a = ")
+b = input("b = ")
+while not(a.isdigit() and b.isdigit()):
+  a = input("a = ")
+  b = input("b = ")
+print("Zhayaby = ", (int(a) + int(b)))
